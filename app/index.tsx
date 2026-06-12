1fb74897b86656d87d1a6b038a2fc025cd1f0c4e
Wendell-Jarquin <ferrufinotania2004@gmail.com>
2026-06-11 20:13:14 -0600

    gg

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { Text } from "react-native";
 
 export default function IndexScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-light-background dark:bg-dark-background">
+
+    <ThemedView margin>
+   
       <Text className="text-3xl font-bold text-light-text dark:text-dark-text">
         HOLA MUNDO
       </Text>
-    </View>
+  
+
+    </ThemedView>
   );
 }
