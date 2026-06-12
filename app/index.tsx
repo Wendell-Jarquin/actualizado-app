a809f9df6025d8bc95ec425e9c21853033b75da1
Wendell-Jarquin <ferrufinotania2004@gmail.com>
2026-06-11 19:22:37 -0600

    g

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { DarkTheme, DefaultTheme, ThemeProvider, } from '@react-navigation/native';
 import { Text, View } from 'react-native';
 
-
+import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import '../globals.css';
+
 
 
 export default function RootLayout() {
@@ -16,6 +17,7 @@ export default function RootLayout() {
       <View className="bg-light-background dark:bg-dark-background flex-1 items-center justify-center">
         <Text className="mt-10 text-3xl text-light-text dark:text-dark-text">Hola Mundo</Text>
       </View>
+      <StatusBar style="auto" />
     </ThemeProvider>
   );
 
