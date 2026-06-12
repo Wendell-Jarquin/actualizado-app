fb8528e41c7734578d106325a5fda5059a139328
Wendell-Jarquin <ferrufinotania2004@gmail.com>
2026-06-11 20:09:39 -0600

    gg

--- presentation/shared/ThemedView.tsx
+++ presentation/shared/ThemedView.tsx
@@ -1,4 +1,5 @@
 import { View, type ViewProps } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useThemeColor } from '@/hooks/useThemeColor'
 
 interface Props extends ViewProps {
@@ -8,14 +9,24 @@ interface Props extends ViewProps {
   bgColor?: string
 }
 
-export default function ThemedView({ className, margin, safe, bgColor, style, ...rest }: Props) {
-  const backgroundColor = bgColor ?? useThemeColor({}, 'background')
+export default function ThemedView({ className, margin = false, safe = false, bgColor, style, children, ...rest }: Props) {
+  const themeBackgroundColor = useThemeColor({}, 'background')
+  const backgroundColor = bgColor ?? themeBackgroundColor
+  const safeArea = useSafeAreaInsets()
 
   return (
     <View
+      style={[{
+        backgroundColor,
+        flex: 1,
+        paddingTop: safe ? safeArea.top : 0,
+        paddingBottom: safe ? safeArea.bottom : 0,
+        marginHorizontal: margin ? 16 : 0,
+      }, style]}
       className={className}
-      style={[{ backgroundColor }, style]}
       {...rest}
-    />
+    >
+      {children}
+    </View>
   )
 }
